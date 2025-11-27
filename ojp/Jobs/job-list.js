67e27eedb39b2c36b5dd9c29d0e1ab5d96da1614
Niharika <niharikavivek2004.in>
2025-11-27 20:04:58 +0530

    first commit

---
+++ ojp/Jobs/job-list.js
@@ -0,0 +1,34 @@
+const jCategory = [
+    {
+        index: 0,
+        image: "../images/google.png",
+        title: "Web Developer",
+        rate: '$900-1200/m',
+        av: "Full Time",
+        companyName: "Google",
+        location: "USA",
+        vacancy: "01",
+        hours: "50h / Week",
+        description: "At Google, we are passionate about building the next generation of web experiences. As a Web Developer, you will play a critical role in creating, maintaining, and improving Google’s web-based platforms and services, providing seamless and fast web experiences for millions of users around the globe.The ideal candidate is skilled in both front-end and back-end web development and is excited to work on some of the world’s most complex web challenges. You will collaborate closely with product managers, designers, and engineers to develop scalable and high-performance web applications.",
+        workplace:"Work from home",
+        education: "Bachelor degree in any Computer Science Course ",
+        experience: "2 to 5years"
+    },
+    {
+        index: 1,
+        image: "../images/uber.png",
+        title: "Business Associate",
+        rate: '$900-1200/m',
+        av: "Part Time",
+        companyName:"Uber",
+        location:"USA",
+        vacancy: "04",
+        hours: "40h / week",
+        description: "As a Business Associate at Uber, you will play a key role in driving the company’s growth and operational efficiency. You’ll work with cross-functional teams, analyze complex business problems, and develop data-driven strategies to optimize processes. Your insights and recommendations will help shape Uber’s business decisions, streamline operations, and improve overall performance in the market. We are looking for a highly motivated individual who thrives in fast-paced environments, has a knack for problem-solving, and can manage multiple projects simultaneously. This role is ideal for someone who is passionate about scaling innovative business models and making a real impact.",
+        workplace: "Work from office",
+        education: "Degree in any Computer Science Course.",
+        experience: "1 to 5 years",
+
+
+    },
+];
